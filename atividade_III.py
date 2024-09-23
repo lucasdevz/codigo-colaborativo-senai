d2222a00f50da5c44439e80eb84498a4bc33f862
lucas <85170796+lucasdevz@users.noreply.github.com>
2024-09-23 17:49:03 +0000

    salvar

---
+++ atividade_III.py
@@ -0,0 +1,22 @@
+"""
+# Identificar arquivos para salvar (commit)
+git status
+
+# Adicionar arquivos para salvar (commit)
+git add .
+
+# Realizando salvamento (commit)
+git commit -m "mensagem"
+
+# Atualiza/traz seu repositório local em relação a repositório remoto.
+git pull
+
+# Envia seu código para o repositório remoto(github)
+
+"""
+
+import os
+os.system("cls || clear")
+
+print("Atividade - III")
+print("Feito por: colaborador 1")
